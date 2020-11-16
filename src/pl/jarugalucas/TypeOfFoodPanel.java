2e2baf291bae9668d94744d21b4c85c57578f9e7
Lucas Jaruga <lucas.jaruga@outlook.com>
2020-11-16 20:39:04 +0100

    redesigned buttons to choose type of food to calculate. Closes #10

--- src/pl/jarugalucas/TypeOfFoodPanel.java
+++ src/pl/jarugalucas/TypeOfFoodPanel.java
@@ -7,8 +7,7 @@ import java.awt.event.ActionListener;
 
 public class TypeOfFoodPanel extends JPanel implements ActionListener {
 
-    private JButton dryButton;
-    private JButton wetButton;
+    private JButton dryWetButton;
     private JButton mixButton;
     private JLabel chooseTypeOfFoodText;
     private JLabel step1Text;
@@ -26,28 +25,21 @@ public class TypeOfFoodPanel extends JPanel implements ActionListener {
         step1Text.setForeground(Color.green);
 
         //TypeOfFood text settings
-        chooseTypeOfFoodText = new JLabel("Choose the type of food to calculate");
-        chooseTypeOfFoodText.setBounds(45, 30, 290, 40);
+        chooseTypeOfFoodText = new JLabel("CHOOSE THE TYPE OF FOOD TO CALCULATE");
+        chooseTypeOfFoodText.setBounds(30, 30, 290, 40);
         chooseTypeOfFoodText.setForeground(Color.orange);
 
         //dry food button settings
-        dryButton = new JButton();
-        dryButton.setText("dry food");
-        dryButton.setBounds(100,70, 90, 40);
-        dryButton.setFocusable(false);
-        dryButton.addActionListener(this);
-
-        //wet food button settings
-        wetButton = new JButton();
-        wetButton.setText("wet food");
-        wetButton.setBounds(100,125, 90, 40);
-        wetButton.setFocusable(false);
-        wetButton.addActionListener(this);
+        dryWetButton = new JButton();
+        dryWetButton.setText("Dry / wet food");
+        dryWetButton.setBounds(88,70, 115, 40);
+        dryWetButton.setFocusable(false);
+        dryWetButton.addActionListener(this);
 
         //mix food button settings
         mixButton = new JButton();
-        mixButton.setText("mix food");
-        mixButton.setBounds(100,180, 90, 40);
+        mixButton.setText("Mix food");
+        mixButton.setBounds(100,120, 90, 40);
         mixButton.setFocusable(false);
         mixButton.addActionListener(this);
 
@@ -55,8 +47,7 @@ public class TypeOfFoodPanel extends JPanel implements ActionListener {
 
         this.add(step1Text);
         this.add(chooseTypeOfFoodText);
-        this.add(dryButton);
-        this.add(wetButton);
+        this.add(dryWetButton);
         this.add(mixButton);
 
     }
--- src/pl/jarugalucas/UserDataPanel.java
+++ src/pl/jarugalucas/UserDataPanel.java
@@ -31,20 +31,20 @@ public class UserDataPanel extends JPanel implements ActionListener {
         // ------------------- text labels start -------------------------- //
 
         //Step2 text settings
-        step2Text = new JLabel("Step 2) Wprowadz dane dot. kota i karmy");
-        step2Text.setBounds(15, 10, 300, 40);
+        step2Text = new JLabel("Step 2) PROVIDE INFORMATION ABOUT CAT AND FOOD");
+        step2Text.setBounds(15, 10, 320, 40);
         step2Text.setForeground(Color.green);
         step2Text.setVisible(false);
 
         //waga kota text settings
-        catWeightLabel = new JLabel("weight of cat");
+        catWeightLabel = new JLabel("WEIGHT OF CAT");
         catWeightLabel.setBounds(15, 35, 100, 40);
         catWeightLabel.setForeground(Color.yellow);
         catWeightLabel.setVisible(false);
 
         //ilosc mokrej text settings
-        wetFoodLabel = new JLabel("ilosc mokrej");
-        wetFoodLabel.setBounds(15, 85, 100, 40);
+        wetFoodLabel = new JLabel("HOW MUCH WET FOOD");
+        wetFoodLabel.setBounds(15, 85, 130, 40);
         wetFoodLabel.setForeground(Color.yellow);
         wetFoodLabel.setVisible(false);
 
@@ -76,12 +76,14 @@ public class UserDataPanel extends JPanel implements ActionListener {
         wetMealType.setBounds(15, 165, 70, 20);
         wetMealType.setVisible(false);
         wetMealType.setFocusable(false);
+        wetMealType.addActionListener(this);
 
         dryMealType = new JRadioButton();
         dryMealType.setText("DRY");
         dryMealType.setBounds(94, 165, 70, 20);
         dryMealType.setVisible(false);
         dryMealType.setFocusable(false);
+        dryMealType.addActionListener(this);
 
         ButtonGroup mealTypeGroup = new ButtonGroup();
         mealTypeGroup.add(wetMealType);
@@ -96,14 +98,14 @@ public class UserDataPanel extends JPanel implements ActionListener {
         // wynik obliczen
         resultOneFoodText = new JLabel();
         resultOneFoodText.setBounds(15, 300, 200, 30);
-        resultOneFoodText.setText("Wynik to: " + "40" + " g");
+        resultOneFoodText.setText("Result is: " + "40" + " g");
         resultOneFoodText.setForeground(Color.yellow);
         resultOneFoodText.setFont(new Font("Calibri", Font.BOLD, 20));
         resultOneFoodText.setVisible(false);
 
         resultMixFoodText = new JLabel();
         resultMixFoodText.setBounds(15, 300, 200, 30);
-        resultMixFoodText.setText("Wynik to: " + "60" + " g");
+        resultMixFoodText.setText("Result is: " + "60" + " g");
         resultMixFoodText.setForeground(Color.yellow);
         resultMixFoodText.setFont(new Font("Calibri", Font.BOLD, 20));
         resultMixFoodText.setVisible(false);
@@ -112,9 +114,9 @@ public class UserDataPanel extends JPanel implements ActionListener {
 
 
         // Calculate button settings
-        calculateButton = new JButton("Oblicz");
+        calculateButton = new JButton("Calculate");
         calculateButton.setFocusable(false);
-        calculateButton.setBounds(40, 260, 70, 30);
+        calculateButton.setBounds(25, 260, 120, 30);
         calculateButton.setVisible(false);
         calculateButton.addActionListener(this);
 
@@ -146,10 +148,11 @@ public class UserDataPanel extends JPanel implements ActionListener {
 
         if(source == calculateButton){
             //TODO
+        } else if(source == dryMealType) {
+
+        } else if(source == wetMealType) {
+
         }
-
-
-
     }
 
 
